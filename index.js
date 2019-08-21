aed3fcf77a9f0bc3c8bc8b1cf291c91af6dfacd5
Austin Smith <trunk9slamchest@gmail.com>
2019-08-21 18:14:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,42 +1,87 @@
+// var katzDeli = [];
+// var otherDeli = ["Steven", "Blake", "Avi"];
+
 function takeANumber(deli, name) {
-  deli.push(name)
-	return `Welcome, ${deli[deli.length - 1]}. You are number ${deli.length} in line.`
+
+  deli.push(name);
+
+	return `Welcome, ${name}. You are number ${deli.length} in line.`;
 }
 
-// function nowServing(deliLine) {
-//   var new_arr = [];
-//     while (deliLine.length > 0) {
-//         new_arr.push(deliLine.shift());
-//         return `Currently serving ${new_arr[new_arr.length - 1]}.`;
-//       }
-//       if (deliLine.length ===0) {
-//         return 'There is nobody waiting to be served!';
-//       }
-// }
+// console.log(takeANumber(katzDeli, 'Ada'));
+// console.log(takeANumber(katzDeli, 'Grace'));
+// console.log(takeANumber(katzDeli, 'Kent'));
+// console.log(takeANumber(["James", "Mark", "Cindy", "Fido"], "Ada"))
 
 function nowServing(deliLine) {
-  if (deliLine.length > 0) {
-   var deliLine_remove = deliLine.shift()
-   return `Currently serving ${deliLine_remove}.`
+
+  if (deliLine.length === 0) {
+    return "There is nobody waiting to be served!";
   }
   else {
-    return "There is nobody waiting to be served!"
+    let deliLine_remove = deliLine.shift();
+    return `Currently serving ${deliLine_remove}.`;
   }
+
 }
+
+// console.log(nowServing(katzDeli))
+// console.log(nowServing(otherDeli))
+// console.log(nowServing([]))
+// console.log(nowServing(["James", "Mark", "Cindy", "Fido"]))
 
 function currentLine(deliLine) {
-  var i = 0
-  var tally = ''
+  let name_str = '';
+  let i = 0;
+
   while (i < deliLine.length) {
     if (i > 0) {
-      tally += ', '
+      name_str += ', ';
     }
-    tally += `${i + 1}. ${deliLine[i]}`
-  i++
+    name_str += `${i + 1}. ${deliLine[i]}`;
+  i++;
   }
+
     if (deliLine.length === 0) {
-      return 'The line is currently empty.'
+      return 'The line is currently empty.';
     }
 
-  return 'The line is currently: ' + tally
+  return 'The line is currently: ' + name_str;
 }
+
+// console.log(currentLine(katzDeli))
+// console.log(currentLine(otherDeli))
+// console.log(currentLine(["Bill", "Jane", "Ann"]))
+// console.log(currentLine([]))
+// console.log(currentLine(["James", "Mark", "Cindy", "Fido"]))
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// function currentLine(deliLine) {
+//   let name_str = '';
+//   let i = 0;
+
+//     if (deliLine.length > 0) {
+//       name_str += 'The line is currently: ';
+//       while (i < deliLine.length) {
+//         if (deliLine.length === 1 || i === deliLine.length - 1) {
+//           name_str += `${i + 1}. ${deliLine[i]}`;
+//         }
+//         else {
+//           name_str += `${i + 1}. ${deliLine[i]}, `;
+//         }
+//       i++;
+//       }
+//     }
+//     else  {
+//       name_str = 'The line is currently empty.';
+//     }
+
+//   return name_str;
+// }
+
+// console.log(currentLine(katzDeli))
+// console.log(currentLine(otherDeli))
+// console.log(currentLine(["Bill", "Jane", "Ann"]))
+// console.log(currentLine([]))
+// console.log(currentLine(["James", "Mark", "Cindy", "Fido"]))
